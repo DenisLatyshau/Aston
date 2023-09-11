f40cbe74fb18401e6e7192a3604e6e3048bb55d7
Denis Latyshau <98422374+DenisLatyshau@users.noreply.github.com>
2023-09-11 11:12:49 +0300

    Revert "feat: add first homeWork"

--- homeWork_01.js
+++
@@ -1,8 +0,0 @@
-const res = "B" + "a" + (1 - "hello");
-console.log(res); // BaNaN
-
-const res2 = (true && 3) + "d";
-console.log(res2); // 3d
-
-const res3 = Boolean(true && 3) + "d";
-console.log(res3); // trued
